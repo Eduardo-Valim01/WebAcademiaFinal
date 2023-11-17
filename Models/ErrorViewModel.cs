49fe586c4522308db7ca6d7f26cf7a2541d14f0f
Eduardo Valim Guedes <2211550511@fema.edu.br>
2023-11-16 21:52:08 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAcademiaFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
